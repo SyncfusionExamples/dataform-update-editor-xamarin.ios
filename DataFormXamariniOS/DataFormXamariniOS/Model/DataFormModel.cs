03e3b0738ebe2c6cc96700942e5f661a656ae626
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2020-04-24 17:40:51 +0530

    XAMARIN-33495 - Sample and screenshot added.

---
+++ DataFormXamariniOS/DataFormXamariniOS/Model/DataFormModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace DataFormXamariniOS
+{
+    public class DataFormModel : INotifyPropertyChanged
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Password { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public string Address { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertychanged(string propertyName)
+        {
+            if (this.PropertyChanged != null)
+            {
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ DataFormXamariniOS/DataFormXamariniOS/ViewController.cs
@@ -0,0 +1,51 @@
+﻿using CoreGraphics;
+using Foundation;
+using Syncfusion.iOS.DataForm;
+using System;
+using UIKit;
+
+namespace DataFormXamariniOS
+{
+    public partial class ViewController : UIViewController
+    {
+        SfDataForm dataForm;
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+
+            UIStackView stack = new UIStackView();
+            dataForm = new SfDataForm();
+            dataForm.DataObject = new DataFormModel();
+            dataForm.Frame = new CGRect(0, 100, 400, 1000);
+
+            UIButton button = new UIButton();
+            button.SetTitle("Submit", UIControlState.Normal);
+            button.TouchUpInside += Button_TouchUpInside;
+            button.Frame = new CGRect(0, 40, 400, 50);
+            button.BackgroundColor = UIColor.Gray;
+            stack.AddSubview(button);
+            stack.AddSubview(dataForm);
+
+            this.View.AddSubview(button);
+            this.View.AddSubview(dataForm);
+        }
+
+        private void Button_TouchUpInside(object sender, EventArgs e)
+        {
+            var model = dataForm.DataObject as DataFormModel;
+            model.Name = "John";
+            dataForm.UpdateEditor("Name");
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
